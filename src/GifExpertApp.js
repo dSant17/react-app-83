ebedcfad19eaf38f3c9ff58226f8b54f35388693
Dan Santana <2019030386@upsin.edu.mx>
2022-03-14 10:18:29 -0700

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import AddCategoria from './components/AddCategoria';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+    // const categorias = ["Milan", "Paris", "Madrid"];
+    const [categorias, setCategorias] = useState(["Milán"]);
+    /*
+    const handleAdd = () => {
+        setCategorias([...categorias, "Londres"]);
+    }
+    */
+    return (
+    <>
+        <h2>GifExpertApp</h2>
+        <AddCategoria setCategorias = { setCategorias }/>
+        <hr/>
+
+        <ol>
+        {
+            categorias.map( categoria => {
+                return <GifGrid
+                    key = { categoria }
+                    categoria = { categoria }
+               />
+            })
+        }
+        </ol>
+    </>
+    )
+}
